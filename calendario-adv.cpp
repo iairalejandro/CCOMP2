b8d9d839d1bd397cdab62808d18dc1ca43dc9af2
iairalejandro <101744230+iairalejandro@users.noreply.github.com>
2024-03-16 09:50:17 -0500

    Create calendario-adv.cpp

---
+++ calendario-adv.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+
+void mostrar_cal(unsigned long dias, int n) {
+    int i;
+    int fil = 5;
+    int col;
+    int d;
+
+    cout << "\tDomingo  Lunes  Martes  Miercoles  Jueves  Viernes  Sabado\n";
+
+    for (i = 0; i <= n; i++, dias++) {
+        // dias%7 -31,30,28o29
+    }
+    
+}
+
+
+int main()
+{
+    
+    cout << "\tDomingo  Lunes  Martes  Miercoles  Jueves  Viernes  Sabado\n";
+
+
+
+}
