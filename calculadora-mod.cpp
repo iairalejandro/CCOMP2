7b3a33aa20dfd2e4ef93c0dee762281d87f1a49d
iairalejandro <101744230+iairalejandro@users.noreply.github.com>
2024-03-23 09:45:24 -0500

    Create calculadora-mod.cpp

---
+++ calculadora-mod.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+int main() {
+	int n1;
+	int n2;
+	int result;
+	cout << "\t\t\t\t\tCALCULADORA MODULAR\n" << endl;
+	cout << "Ingrese el primer numero: ";
+	cin >> n1;
+	cout << "Ingrese el segundo numero: ";
+	cin >> n2;
+	result = n1 % n2;
+	cout << result;
+
+
+}
