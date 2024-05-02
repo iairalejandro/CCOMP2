62c246d328869e0f76c8cda881c9ca70c69ef267
iairalejandro <101744230+iairalejandro@users.noreply.github.com>
2024-05-02 17:02:23 -0500

    Update binsearch.cpp

--- binsearch.cpp
+++ binsearch.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
 int binarySearch(int *array, int size, int target) {
     int izq = 0;
     int der = size - 1;
 
     while (izq <= der) {
-        int mid = izq + (der - izq) / 2;
+        int *mid = array + (izq + (der - izq) / 2);
 
-        if (array[mid] == target) {
-            return mid;
+        if (*mid == target) {
+            return mid - array;
         }
 
-        if (array[mid] < target) {
-            izq = mid + 1;
-        }
-
-        else {
-            der = mid - 1;
+        if (*mid < target) {
+            izq = mid - array + 1;
+        } else {
+            der = mid - array - 1;
         }
     }
     return -1;
@@ -27,7 +24,7 @@ int binarySearch(int *array, int size, int target) {
 int main() {
     int array[] = {1, 3, 5, 7, 9, 11, 13, 14, 17, 19};
     int size = sizeof(array) / sizeof(array[0]);
-    int target = 4;
+    int target = 5;
 
     int result = binarySearch(array, size, target);
     if (result != -1) {
